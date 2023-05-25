9616cb3aee58ebad946175f5dc9345f153dc00e1
Lea Henckel <lea.henckel@outlook.com>
2023-05-25 14:01:19 +0200

    changes does not work

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,4 +1,4 @@
-const gallery = (function () {
+/* const gallery = (function () {
     const GALLERY = document.querySelector(".gallery__container");
     const FOCUSED_IMAGE = document.createElement("img");
     FOCUSED_IMAGE.classList.add("gallery__focusedImage");
@@ -40,4 +40,81 @@ const gallery = (function () {
     return {
       init,
     };
-  })();
+  })(); */
+
+    // Ovenstående viser billederne vertikalt
+
+
+
+
+  
+
+
+
+    const gallery = (function () {
+    const GALLERY = document.querySelector(".gallery__container");
+    const FOCUSED_IMAGE = document.createElement("img");
+    FOCUSED_IMAGE.classList.add("gallery__focusedImage");
+
+  
+  
+    function createHouseSection(house, images) {
+      const houseLabel = document.createElement("div");
+      houseLabel.textContent = house;
+      houseLabel.classList.add("gallery__houseLabel");
+  
+      const houseContainer = document.createElement("div");
+      houseContainer.classList.add("gallery__houseContainer");
+      houseContainer.append(houseLabel);
+  
+      const thumbnailsContainer = document.createElement("div");
+      thumbnailsContainer.classList.add("gallery__thumbnails");
+  
+      let thumbnailCount = 0;
+      let rowContainer = null;
+  
+      images.forEach((image) => {
+        if (thumbnailCount % 7 === 0) {
+          rowContainer = document.createElement("div");
+          rowContainer.classList.add("gallery__row");
+          thumbnailsContainer.append(rowContainer);
+        }
+  
+        const BUTTON = document.createElement("button");
+        BUTTON.addEventListener("mouseover", changeFocus);
+        BUTTON.innerHTML = `<img src="${image}" alt="gallery thumbnail" class="gallery__thumbnail">`;
+        BUTTON.classList.add("gallery__button");
+        rowContainer.append(BUTTON);
+  
+        thumbnailCount++;
+      });
+  
+      houseContainer.append(thumbnailsContainer);
+      return houseContainer;
+    }
+  
+    function changeFocus(event) {
+      if (event.target.className == "gallery__thumbnail") {
+        FOCUSED_IMAGE.src = event.target.src;
+      }
+    }
+  
+    function init(houses) {
+      for (const house in houses) {
+        const houseSection = createHouseSection(house, houses[house]);
+        GALLERY.append(houseSection);
+      }
+  
+      GALLERY.append(FOCUSED_IMAGE);
+    }
+  
+    return {
+      init,
+    };
+  })();  
+
+
+
+
+
+
